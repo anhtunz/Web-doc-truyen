1b29957d5783fb69683f04319bc71322098e5766
anhtunz <anhtuanmk62@gmail.com>
2023-05-16 12:01:42 +0700

    Them may truyen thuoc the loai truyen cuoi co tinh_trang = full

---
+++ SQL_Script/add_truyen_moi.sql
@@ -0,0 +1,75 @@
+﻿INSERT INTO dbo.truyen  VALUES(
+17,
+N'Nguyên Nhân Học Trò Không Thích Môn Lịch Sử',
+5,
+CAST(N'2023-05-16' AS Date),
+4, 
+N'',
+1, 
+N'Full', 
+'https://webtruyen.com/public/images/aassnguyennhanhoctrokhongthichmonlichsuDsHUgn6AlQ.jpg'
+)
+
+INSERT INTO dbo.truyen  VALUES(
+18,
+N'Cô Giáo Muốn Gặp Bố',
+5,
+CAST(N'2023-05-16' AS Date),
+4, 
+N'',
+1, 
+N'Full', 
+'https://webtruyen.com/public/images/aasscogiaomuongapbo3flceQXnp6.jpg'
+)
+
+INSERT INTO dbo.truyen  VALUES(
+19,
+N'Tác Hại Khôn Lường Của Việc Mở Miệng Lúc Ngủ',
+5,
+CAST(N'2023-05-16' AS Date),
+4, 
+N'',
+1, 
+N'Full', 
+'https://webtruyen.com/public/images/aasstachaikhonluongcuaviecmomienglucngutV2wmNLv3j.jpg'
+)
+
+INSERT INTO dbo.truyen  VALUES(
+20,
+N'Vua Nói Dối Đi Câu Cá',
+5,
+CAST(N'2023-05-16' AS Date),
+4, 
+N'',
+1, 
+N'Full', 
+'https://webtruyen.com/public/images/aassvuanoidoidicaucaCG9utoTiNI.jpg'
+)
+
+INSERT INTO dbo.truyen  VALUES(
+21,
+N'Nhổ Răng Không Đau',
+5,
+CAST(N'2023-05-16' AS Date),
+4, 
+N'',
+1, 
+N'Full', 
+'https://webtruyen.com/public/images/aassnhorangkhongdauudaIQ111Gh.jpg'
+)
+
+INSERT INTO dbo.truyen  VALUES(
+22,
+N'Bị Sa Thải Vì Hoàn Thành 200% Công Việc',
+5,
+CAST(N'2023-05-16' AS Date),
+4, 
+N'',
+1, 
+N'Full', 
+'https://webtruyen.com/public/images/aassbisathaivihoanthanh200congvieclxt2fR5IoX.jpg'
+)
+
+
+
+
